eeee6538ca508b0d4818108e7a05e18365569ede
FM 5092 <72297857+TianyuZhou1123@users.noreply.github.com>
2021-02-24 21:56:50 +0800

    Update OptionPrice+std.cs

--- MonteCarloSimulation_1/OptionPrice+std.cs
+++ MonteCarloSimulation_1/OptionPrice+std.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,7 +19,7 @@ namespace MonteC
         public double Sigma { get { return sigma; } set { sigma = value; } }
         //Sigma means Volitility
         public double T { get { return t; } set { t = value; } }
-        //T(Tenor) means the time to hold
+        //T(Tenor) means the time to hold(year)
         public int Sims { get { return trials; } set { trials = value; } }
         //Sims means the trials of Matro Carlo Simulations
         public int Steps { get { return steps; } set { steps = value; } }
@@ -30,13 +28,12 @@ namespace MonteC
         //IsCall means to judge if it is a call option
         public double[,] Epsilon { get { return randomnumber; } set { randomnumber = value; } }
         //Epsilon means generate the random numbers
-        public static double std(int Sims, double[] Price)
+        public static double std(int Sims, double[] Price) //calculate std
         {
             double P = Price.Average();
             double[] sum = new double[Sims];
             for (int i = 0; i < Sims; i++)
                 sum[i] = (Price[i] - P) * (Price[i] - P);
-            //calculate std
             double sd = Math.Sqrt((sum.Sum()) / (Sims - 1));
             return sd;
         }
@@ -45,14 +42,13 @@ namespace MonteC
             double[,] allsims = new double[Sims, Steps + 1];
             for (int i = 0; i < Sims; i++)
             {
-                allsims[i, 0] = S;
-                //let the first column be S
+                allsims[i, 0] = S; //let the first column be S
             }
             for (int i = 0; i < Sims; i++)
             {
                 for (int j = 0; j < Steps; j++)
                 {
-                    allsims[i, j + 1] = allsims[i, j] * Math.Exp((Mu - Sigma * Sigma / 2) * (T / Steps) + Sigma * Math.Sqrt(T / Steps) * Epsilon[i, j]);
+                    allsims[i, j + 1] = allsims[i, j] * Math.Exp((Mu - Sigma *Sigma / 2) * (T / Steps) + Sigma * Math.Sqrt(T / Steps) * Epsilon[i, j]);
                 }
             }//store the prices of every steps
             double[] value = new double[Sims];
@@ -70,12 +66,12 @@ namespace MonteC
                     value[i] = Math.Max(K - allsims[i, Steps], 0);
                 }
             }
-            double optionprice = value.Sum() / Sims * Math.Exp(-Mu * T);
+            double optionprice = value.Average() * Math.Exp(-Mu * T);
             //calculate the option price in the beginning
             double[] Price = new double[Sims];
             for (int i = 0; i < Sims; i++)
                 Price[i] = value[i] * Math.Exp(-Mu * T);
-            double sd = EuropeanOption.std(Sims, Price);
+            double sd = EuropeanOption.std(Sims,Price);
             double[] result = { optionprice, sd };
             return result;
         }
